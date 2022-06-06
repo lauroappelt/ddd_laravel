a6b9ae7e382fffdc2ab77acb5198bdd1fae9facb
lauro <lauro.appelt@universo.univates.br>
2022-06-06 12:22:03 -0300

    env

---
+++ app/App/Web/Task/Controllers/TaskController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Web\Task\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Web\Task\Requests\TaskRequest;
+use Domain\Task\Actions\CreateTaskAction;
+use Domain\Task\DataTransferObjects\TaskData;
+use Domain\Task\Models\Task;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = app(Task::class)->get();
+        return view('welcome', compact('tasks'));
+    }
+
+    public function store(TaskRequest $taskRequest, CreateTaskAction $action)
+    {
+        $data = TaskData::fromRequest($request);
+        $response = $action($data);
+    }
+}
---
+++ app/App/Web/Task/Requests/TaskRequest.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Web\Task\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TaskRequest extends FormRequest
+{
+    public function rules(){
+        return [];
+    }
+}
